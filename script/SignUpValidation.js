12bdcda5b7be1440ba30438e855a3df28840ca23
FjollaNebihi <fn70380@ubt-uni.net>
2024-12-06 13:38:05 +0100

    validimi sign up(updated)

--- script/SignUpValidation.js
+++ script/SignUpValidation.js
@@ -4,37 +4,39 @@ function validateForm(){
   let username = document.getElementById("username").value;
   let email=document.getElementById("email").value;
   let password=document.getElementById("password").value;
+  let error=document.getElementById("error");
 
   let password_length=password.length;
-
-  if(name.length=0 ){
-    alert("Please enter the requested information!");
+  error1.innerHTML="";
+  error2.innerHTML="";
+  error3.innerHTML="";
+  error4.innerHTML="";
+  error5.innerHTML="";
+  
+  if(name.length==0 ){
+    error1.innerHTML="Please enter your first name!";
     return false;
   }
-  if(lastname="" ){
-    alert("Please enter the requested information!");
+  else if(lastname=="" ){
+    error2.innerHTML="Please enter your last name!";
     return false;
   }
-  if(emailname="" ){
-    alert("Please enter the requested information!");
+  else if(username=="" ){
+    error3.innerHTML="Please create a username!";
     return false;
   }
-  if(username="" ){
-    alert("Please enter the requested information!");
+  else if(email==="" ){
+    error4.innerHTML="A valid email address is requested!";
     return false;
   }
-  if(name="" ){
-    alert("Please enter the requested information!");
-    return false;
-  }
-else if(password_length<8){
-    alert("Password should have at least 8 characters!");
+ 
+if(password_length<8){
+  error5.innerHTML="Password should have at least 8 characters";
+  return false;
   }
   
   else{
     alert("Your account has been successfully created!");
     return true;
   }
-
-
 }
