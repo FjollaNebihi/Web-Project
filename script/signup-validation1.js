877a89f885d9dcbc12280e9a7aec9ada9000cf05
FjollaNebihi <fn70380@ubt-uni.net>
2025-01-25 22:38:50 +0100

    Krijimi i databazes ne myadmin dhe lidhja e databazes me metoden PDO

--- script/signup-validation1.js
+++
@@ -1,9 +0,0 @@
-function SignUp(){
-    let email = document.getElementById("input").value;
-    error1.innerHTML="";
-
-    if (email=="" ){
-        error1.innerHTML="A valid email address is requested!";
-        return false;
-    }
-}
