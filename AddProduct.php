940bbb211f948dca827ea6c3aaf8794f743b5c2a
DiolaSokoli <ds71278@ubt-uni.net>
2025-02-01 06:47:32 +0100

    adding product registration form

--- AddProduct.php
+++ AddProduct.php
@@ -1,4 +1,4 @@
-qetu e bon njejt si te register form qe kur te shkruhen tdhenat dalin direkt ndatabaze
+
 <?php
 include_once 'AddProduct.php';
 class Product1{
@@ -52,3 +52,36 @@ class Product1{
 }
 }
 ?>
+<!DOCTYPE html>
+<html lang="sq">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Registration Form</title>
+</head>
+<body>
+    <h2>Add products</h2>
+    <form action="upload_product.php" method="post" enctype="multipart/form-data">
+        <label for="Product_Name">Product Name:</label><br>
+        <input type="text" id="Product_Name" name="Product_Name" required><br><br>
+
+        <label for="Brand">Brand:</label><br>
+        <input type="text" id="Brand" name="Brand" required><br><br>
+
+        <label for="Price">Price:</label><br>
+        <input type="number" id="Price" name="Price" step="0.01" required><br><br>
+
+        <label for="Price_OnSale">Price on Sale:</label><br>
+        <input type="number" id="Price_OnSale" name="Price_OnSale"><br><br>
+
+        <label for="Stock">Stock:</label><br>
+        <input type="number" id="Stock" name="Stock" required><br><br>
+
+        <label for="Image">Image:</label><br>
+        <input type="file" id="Image" name="Image" accept="image/*" required><br><br>
+
+        <input type="submit" value="Add products">
+    </form>
+</body>
+</html>
+
--- signup.php
+++ signup.php
@@ -45,16 +45,16 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
       <h2>Create an account</h2>
 
       <form action="#" method="POST" name="SignUp" onsubmit="return validateForm()"> 
-      <input type="text" class="name" name="first_name" id="name" placeholder="First Name">
+      <input type="text" class="name" name="first_name" id="name" placeholder="First Name" required>
       <div id="error1"></div>
-      <input type="text" class="last-name" name="last_name" id="lastname" placeholder="Last Name">
+      <input type="text" class="last-name" name="last_name" id="lastname" placeholder="Last Name" required>
       <div id="error2"></div>
      
-        <input type="text" class="username" name="username" id="username" placeholder="Create a username">
+        <input type="text" class="username" name="username" id="username" placeholder="Create a username" required>
         <div id="error3"></div>
-        <input type="text" class="email" name="email" id="email" placeholder="Email address">
+        <input type="text" class="email" name="email" id="email" placeholder="Email address" required>
         <div id="error4"></div>
-        <input type="password" class="password" name="password" id="password" placeholder="Create a password">
+        <input type="password" class="password" name="password" id="password" placeholder="Create a password" required>
         <div id="error5"></div>
         <button class="submit" onclick="validateForm()">Sign Up</button>
   
